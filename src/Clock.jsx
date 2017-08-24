f90ba7c95fae736f35e40dc7c6a4b46e79ccf49b
Yusuf Hendricks <yhendricks1@gmail.com>
2017-08-24 12:18:17 +0200

    021 Adding Helper Methods with Ternary Expressions

--- src/Clock.jsx
+++ src/Clock.jsx
@@ -30,13 +30,17 @@ class Clock extends Component {
         this.setState({days, hours, minutes, seconds});
     }
 
+    leading0(num) {
+        return num < 10 ? '0' + num : num;
+    }
+
     render(){
         return (
             <div>
-                <div className="Clock-days">{this.state.days} Days</div>
-                <div className="Clock-hours">{this.state.hours} hours</div>
-                <div className="Clock-minutes">{this.state.minutes} minutes</div>
-                <div className="Clock-seconds">{this.state.seconds} seconds</div>
+                <div className="Clock-days">{this.leading0(this.state.days)} Days</div>
+                <div className="Clock-hours">{this.leading0(this.state.hours)} hours</div>
+                <div className="Clock-minutes">{this.leading0(this.state.minutes)} minutes</div>
+                <div className="Clock-seconds">{this.leading0(this.state.seconds)} seconds</div>
             </div>
         )
     }
